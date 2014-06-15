5e8bb0d0facced6ab0a16f6a3420ee4fa532801a
Matthew M. Boedicker <matthewm@boedicker.org>
2014-06-14 21:25:12 -0700

    Add DockerFile class.

---
+++ lib/bosh_job_docker/docker_file.rb
@@ -0,0 +1,56 @@
+require 'stringio'
+
+module BoshDockerJob
+
+  class DockerFile
+
+    def initialize
+      @buffer = StringIO.new
+    end
+
+    def apt_get(packages)
+      packages.sort.each do |package|
+        run("apt-get install -y #{package}")
+      end
+      blank_line
+    end
+
+    def add(src, dest)
+      line("ADD #{src} #{dest}")
+    end
+
+    def env(name, value)
+      line("ENV #{name} #{value}")
+    end
+
+    def from(image)
+      line("FROM #{image}")
+      blank_line
+    end
+
+    def run(command)
+      line("RUN #{command}")
+    end
+
+    def blank_line
+      buffer << "\n"
+    end
+
+    def comment(text)
+      line("# #{text}")
+    end
+
+    def write(path)
+      open(path, 'w') { |f| f.write(buffer.string) }
+    end
+
+    private
+
+    def line(line)
+      buffer << line
+      buffer << "\n"
+    end
+
+    attr_reader :buffer
+  end
+end
