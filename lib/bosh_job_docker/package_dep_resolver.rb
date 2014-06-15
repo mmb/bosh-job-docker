8a803912a7eea3a9d9593999c94d5249f05933a9
Matthew M. Boedicker <matthewm@boedicker.org>
2014-06-14 20:57:46 -0700

    Add PackageDepResolver class.

---
+++ lib/bosh_job_docker/package_dep_resolver.rb
@@ -0,0 +1,21 @@
+module BoshJobDocker
+  class PackageDepResolver
+    def initialize(release_manifest)
+      @release_manifest = release_manifest
+    end
+
+    def package_deps(package_name)
+      package = lookup_package(package_name)
+      deps = package['dependencies'].map { |p| package_deps(p) }.flatten
+      deps << package_name
+    end
+
+    private
+
+    def lookup_package(package_name)
+      release_manifest['packages'].find { |p| p['name'] == package_name }
+    end
+
+    attr_reader :release_manifest
+  end
+end
