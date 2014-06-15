a9391219378030dbaa8d3a4fa855c3b32f189089
Matthew M. Boedicker <matthewm@boedicker.org>
2014-06-14 21:02:48 -0700

    Nest TarExtracter in module.

--- lib/bosh_job_docker/tar_extracter.rb
+++ lib/bosh_job_docker/tar_extracter.rb
@@ -2,12 +2,12 @@
 
 require 'English'
 
-require 'bosh_job_docker'
-
-class BoshJobDocker::TarExtracter
-  def extract(tarball, dir)
-    pid = spawn('tar', '-C', dir, '-xzf', tarball)
-    Process.wait(pid)
-    raise 'tar command failed' unless $CHILD_STATUS.success?
+module BoshJobDocker
+  class TarExtracter
+    def extract(tarball, dir)
+      pid = spawn('tar', '-C', dir, '-xzf', tarball)
+      Process.wait(pid)
+      fail 'tar command failed' unless $CHILD_STATUS.success?
+    end
   end
 end
